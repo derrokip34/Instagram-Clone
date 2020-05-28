8a36ca862804f5e7b68f179e30e5692e71b7ac70
derrickip34 <derrickip34@gmail.com>
2020-05-28 15:09:07 +0300

    create a home view

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+
+    title = 'Welcome to Instagram'
+    return render(request, 'index.html',{'title':title})
