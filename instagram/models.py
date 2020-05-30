1d3bd0fd79d79e989f2fe93b4d3fabcbe0da52f3
derrickip34 <derrickip34@gmail.com>
2020-05-30 16:35:23 +0300

    create a profile page

--- instagram/models.py
+++ instagram/models.py
@@ -9,12 +9,46 @@ class Image(models.Model):
     profile = models.ForeignKey('UserProfile',on_delete=models.CASCADE)
     likes = models.IntegerField(default=0)
 
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def get_profile_images(cls,profile):
+        images = Image.objects.filter(profile__pk = profile)
+        return images
+
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.all()
+        return images
+
 class Comments(models.Model):
     image = models.ForeignKey('Image',on_delete=models.CASCADE)
     comment = models.CharField(max_length=150)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
+    def save_comment(self):
+        self.save()
+
 class UserProfile(models.Model):
     profile_pic = models.ImageField(upload_to='media/')
     bio = models.CharField(max_length=100,default="I'm new to Instagram")
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def search_user(cls,search_term):
+        profiles = cls.objects.filter(user__icintains=search_term)
+        return profiles
+
+    @classmethod
+    def get_by_id(cls,id):
+        profile = UserProfile.objects.get(user=id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls,id):
+        profile = UserProfile.objects.filter(user = id).first()
+        return profile
--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .models import Image,UserProfile
 
 # Create your views here.
 
 def home(request):
+    images = Image.get_all_images()
 
     title = 'Welcome to Instagram'
-    return render(request, 'index.html',{'title':title})
+    return render(request, 'index.html',{'title':title,'images':images})
+
+@login_required()
+def profile(request,username):
+    profile = User.objects.get(username=username)
+    try:
+        profiles = UserProfile.get_by_id(profile.id)
+    except:
+        profiles = UserProfile.filter_by_id(profile.id)
+    images = Image.get_profile_images(profile.id)
+    return render(request, 'profile.html', {'images': images,"profile":profile,"profiles":profiles})
