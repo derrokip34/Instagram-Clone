45bbfe3a205a2ee054cc1795cf81bdb4a2723954
derrickip34 <derrickip34@gmail.com>
2020-06-04 10:38:36 +0300

    run comments tests successfully

--- instagram/tests.py
+++ instagram/tests.py
@@ -65,3 +65,32 @@ class ImageTests(TestCase):
         images = Image.get_all_images()
         self.assertTrue(len(images)>0)
 
+class CommentsTests(TestCase):
+    def setUp(self):
+        self.user = User(id=1,username='derrick',email='derrick@gmail.com',password='58744')
+        self.user.save()
+        self.profile = Profile(user_id=1,bio='Cool cool cool No doubt no doubt no doubt',profile_pic='b99.jpg')
+        self.profile.save()
+        self.img = Image(image='b99.jpg',image_name='Brooklyn 99',image_caption='Brooklyn 99 season 7 Poster',date_posted='2020-06-04',likes=1000,owner=self.user,profile=self.profile)
+        self.img.save_image()
+        self.comment = Comments(image=self.img,comment="Can't wait for the next season of th Nine Nine",user=self.user,date_posted='2020-06-04')
+        self.comment.save_comment()
+
+    def tearDown(self):
+        Comments.objects.all().delete()
+
+    def test_isinstance(self):
+        self.assertTrue(isinstance(self.user,User))
+        self.assertTrue(isinstance(self.profile,Profile))
+        self.assertTrue(isinstance(self.img,Image))
+        self.assertTrue(isinstance(self.comment,Comments))
+
+    def test_save_comment_method(self):
+        self.comment.save_comment()
+        comments = Comments.objects.all()
+        self.assertTrue(len(comments)>0)
+
+    def test_delete_comment(self):
+        self.comment.delete_comment()
+        comments = Comments.objects.all()
+        self.assertTrue(len(comments)==0)
