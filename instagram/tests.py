600ec6bb5fad5117af99642481f81e8e22f640ac
derrickip34 <derrickip34@gmail.com>
2020-06-04 10:30:15 +0300

    run Image tests successfully

--- instagram/tests.py
+++ instagram/tests.py
@@ -33,3 +33,35 @@ class ProfileTests(TestCase):
         self.profile.save_profile()
         profile = Profile.get_by_id(1)
         self.assertEqual(profile.user_id,1)
+
+class ImageTests(TestCase):
+    def setUp(self):
+        self.user = User(id=1,username='derrick',email='derrick@gmail.com',password='58744')
+        self.user.save()
+        self.profile = Profile(user_id=1,bio='Cool cool cool No doubt no doubt no doubt',profile_pic='b99.jpg')
+        self.profile.save()
+        self.img = Image(image='b99.jpg',image_name='Brooklyn 99',image_caption='Brooklyn 99 season 7 Poster',date_posted='2020-06-04',likes=1000,owner=self.user,profile=self.profile)
+        self.img.save_image()
+
+    def tearDown(self):
+        Image.objects.all().delete()
+
+    def test_isinstance(self):
+        self.assertTrue(isinstance(self.user,User))
+        self.assertTrue(isinstance(self.profile,Profile))
+        self.assertTrue(isinstance(self.img,Image))
+
+    def test_save_image_method(self):
+        self.img.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+    def test_delete_image(self):
+        self.img.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)==0)
+
+    def test_get_all_images(self):
+        images = Image.get_all_images()
+        self.assertTrue(len(images)>0)
+
