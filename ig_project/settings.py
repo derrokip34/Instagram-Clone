5b0aeedec678a22a86f5ebaa22fbe26f8cb6cf3b
derrickip34 <derrickip34@gmail.com>
2020-06-04 13:40:55 +0300

    create a user's profile after signing up

--- ig_project/settings.py
+++ ig_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'instagram',
+    'instagram.apps.InstagramConfig',
     'bootstrap4',
     'crispy_forms',
 ]
